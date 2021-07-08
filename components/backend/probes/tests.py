20e9675e29db5de94b927b300b70ca3bd55f5c5b
Gilles Faucherand <gilles.faucherand@gmail.com>
2021-07-08 17:49:45 +0200

    Added pytest

--- components/backend/probes/tests.py
+++ components/backend/probes/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from probes.models import Probe
+
+
+class ProbeTestCase(TestCase):
+    def setUp(self):
+        self.probe_name = "probe_name"
+        self.probe = Probe.objects.create(
+            name=self.probe_name,
+        )
+
+    def test_create_probe(self):
+        self.assertIsInstance(self.probe, Probe)
+        assert self.probe.name == self.probe_name
